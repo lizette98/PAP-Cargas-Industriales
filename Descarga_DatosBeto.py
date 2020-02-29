a304b517a03341222f819533dec7bf36ebd0cc81
AlbertoNuno <anunoguevara@gmail.com>
2020-02-29 09:59:28 -0600

    cambios_Beto

--- Descarga_DatosBeto.py
+++ Descarga_DatosBeto.py
@@ -5,7 +5,7 @@ end_date = "02-21-2020"
 api_key = "48236d9a55ec414e930211450201802"
 #"21.1236,-101.68" leon, guanajuato
 #salamanca, gto : 20.53627,-101.20445
-location_list = ["20.53627,-101.20445"]
+location_list = ["New York"]
 "San_Luis_Potosi"
 
 hist_weather_data = retrieve_hist_data(api_key,
